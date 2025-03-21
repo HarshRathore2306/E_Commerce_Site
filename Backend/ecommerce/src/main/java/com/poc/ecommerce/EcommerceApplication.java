44529cb738f9171e59797d48b649f81c796cebee
Harsh Rathore <harshrathore2306@gmail.com>
2025-03-21 16:46:55 +0530

    First commit

---
+++ Backend/ecommerce/src/main/java/com/poc/ecommerce/EcommerceApplication.java
@@ -0,0 +1,15 @@
+package com.poc.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceApplication.class, args);
+	}
+
+}
+// No additional code is needed at $PLACEHOLDER$ to run this program.
+// You can run the program as it is by executing the main method.
